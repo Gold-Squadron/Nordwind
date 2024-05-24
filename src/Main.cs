6684ce2d9e6d8489b03d40cdbbf7be19edf62a5c
Noah S <noahalexsimon@outlook.de>
2024-05-25 00:10:16 +0200

    Added Main.cs and basic wind control
    Co-Authored-By: Hamadi Bettahar <61559077+Import-Nihilism@users.noreply.github.com>

---
+++ src/Main.cs
@@ -0,0 +1,41 @@
+using Godot;
+
+public class Main : Node {
+
+    [Export]
+    public Vector2 windDir = Vector2.Up;
+
+    public override void _Ready() {
+        GD.Print("Main Level is initializing.");
+        
+    }
+
+    public override void _Process(float delta) {
+        handlePlayerInput(delta);
+    }
+    
+    void handlePlayerInput(float delta) {
+        var desiredDirection = Vector2.Zero;
+
+        if (Input.IsActionPressed("ui_right")) {
+            desiredDirection.x += 1;
+        }
+
+        if (Input.IsActionPressed("ui_left")) {
+            desiredDirection.x -= 1;
+        }
+
+        if (Input.IsActionPressed("ui_down")) {
+            desiredDirection.y += 1;
+        }
+
+        if (Input.IsActionPressed("ui_up")) {
+            desiredDirection.y -= 1;
+        }
+
+        windDir += desiredDirection * delta;
+        windDir = windDir.Normalized();
+        GD.Print(windDir);
+    }
+    
+}
