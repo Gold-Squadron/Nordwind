71a5ae1c64f976eaddaaf96d01c67cf566e75b45
Noah <noahalexsimon@outlook.de>
2024-05-26 11:44:05 +0200

    Added Level Select Screen

---
+++ scenes/levels/LevelLoadButton.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public class LevelLoadButton : Button  {
+    [Export]
+    public PackedScene scene;
+
+    public void setLevel(int x) {
+        scene = ResourceLoader.Load<PackedScene>("res://scenes/levels/Level_" + x + ".tscn");
+        Text = x.ToString();
+    }
+    
+    public override void _Pressed() {
+        GetTree().ChangeSceneTo(scene);
+    }
+}
